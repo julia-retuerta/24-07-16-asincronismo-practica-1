e4580ddff461581d945ada60176bff0c02c4f84d
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-16 15:23:25 +0200

    clase-16-julio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const buttonElement = document.getElementById('button');
+
+const generateAdvice = async url => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+buttonElement.addEventListener('click', () => generateAdvice('https://api.adviceslip.com/advice'));
