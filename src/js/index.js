fe0dff09fa27e77b30d16dc49f418be1e9205bec
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-17 09:50:19 +0200

    clase-17-julio

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,22 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
+const idNumberElement = document.getElementById('id-number');
+const adviceElement = document.getElementById('advice');
 const buttonElement = document.getElementById('button');
 
-const generateAdvice = async url => {
+const generateAdvice = async () => {
   try {
-    const response = await fetch(url);
+    const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
+    idNumberElement.textContent = `ADVICE #${data.slip.id}`;
+    adviceElement.textContent = data.slip.advice;
     console.log(data);
   } catch (error) {
     console.log(error);
   }
 };
 
-buttonElement.addEventListener('click', () => generateAdvice('https://api.adviceslip.com/advice'));
+generateAdvice();
+
+buttonElement.addEventListener('click', generateAdvice);
