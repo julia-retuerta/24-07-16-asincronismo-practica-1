9a5a431be842028e2dc7d0f5981bfba10f363cd4
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-18 12:44:05 +0200

    practica-casa-18-julio

--- src/js/index.js
+++ src/js/index.js
@@ -5,13 +5,18 @@ const idNumberElement = document.getElementById('id-number');
 const adviceElement = document.getElementById('advice');
 const buttonElement = document.getElementById('button');
 
+const printAdvice = (id, advice) => {
+  idNumberElement.textContent = `ADVICE #${id}`;
+  adviceElement.textContent = advice;
+};
+
 const generateAdvice = async () => {
   try {
     const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
-    idNumberElement.textContent = `ADVICE #${data.slip.id}`;
-    adviceElement.textContent = data.slip.advice;
-    console.log(data);
+    const id = data.slip.id;
+    const advice = data.slip.advice;
+    printAdvice(id, advice);
   } catch (error) {
     console.log(error);
   }
